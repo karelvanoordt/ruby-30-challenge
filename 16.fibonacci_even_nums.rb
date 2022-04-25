5e03f8072f8429e66a824955aec163378e3c6c3c
Karel van Oordt <vanoordtkarel@gmail.com>
2022-04-25 10:50:24 -0500

    Solve challenge #16

--- 16.fibonacci_even_nums.rb
+++ 16.fibonacci_even_nums.rb
@@ -1,2 +1,18 @@
 # Given an n number. Calculate the sum of even fibonacci numbers (until nth count)
 
+def fibonacci_even_sum(n)
+    number = 0
+    sequence = []
+    sum = 0
+    (0..n).each do |i|
+        number = i if i <= 1
+        number = sequence[-1] + sequence[-2] if i > 1
+        sequence << number
+
+        sum += number if number % 2 == 0
+    end
+    sum
+end
+
+
+puts fibonacci_even_sum(8)

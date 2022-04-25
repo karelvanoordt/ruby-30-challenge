9b9db3624b7df80b601681475f2b367a4e207b26
Karel van Oordt <vanoordtkarel@gmail.com>
2022-04-25 10:53:15 -0500

    Add comments to challenge #16

--- 16.fibonacci_even_nums.rb
+++ 16.fibonacci_even_nums.rb
@@ -1,15 +1,15 @@
 # Given an n number. Calculate the sum of even fibonacci numbers (until nth count)
 
 def fibonacci_even_sum(n)
-    number = 0
-    sequence = []
-    sum = 0
+    number = 0 # current fibonacci number
+    sequence = [] # store the values, specially the last 2
+    sum = 0 # total sum
     (0..n).each do |i|
         number = i if i <= 1
-        number = sequence[-1] + sequence[-2] if i > 1
-        sequence << number
+        number = sequence[-1] + sequence[-2] if i > 1 # This will calculate the current number summing the last w
+        sequence << number # stores current into sequence
 
-        sum += number if number % 2 == 0
+        sum += number if number % 2 == 0 # adds even number to sequence
     end
     sum
 end
---
+++ 17.fibonacci_even_v2.rb
@@ -0,0 +1,18 @@
+# Given an n number. Calculate the sum of even fibonacci numbers (until nth count)
+
+def fibonacci_even_sum(n) # From last challenge
+    number = 0
+    sequence = []
+    sum = 0
+    (0..n).each do |i|
+        number = i if i <= 1
+        number = sequence[-1] + sequence[-2] if i > 1
+        sequence << number
+
+        sum += number if number % 2 == 0
+    end
+    sum
+end
+
+
+puts fibonacci_even_sum(8)
